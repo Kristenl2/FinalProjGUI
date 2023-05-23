9c88bc016fe0bf97f4c69aca3b3cf993757163d1
1E10 Laptop <sample@bths.edu>
2023-05-23 11:02:32 -0400

    khh

---
+++ src/display.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class display {
+    private JButton pomodoroButton;
+    private JButton shortBreakButton;
+    private JButton longBreakButton;
+    private JButton button4;
+}
